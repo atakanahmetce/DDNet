6381b9a8bf409d54077c93eaf5385c997c083133
Ahmet Atakan <atakanahmetce@gmail.com>
2022-04-13 00:09:13 +0300

    ppi intact data added

--- data_collect/ppi_collection.php
+++ data_collect/ppi_collection.php
@@ -4,23 +4,14 @@ include('functions.php');
 
 $export_file = fopen("../data/ppi_intact.csv","w");
 
-$ppi_data = array();
+for ($i=0; $i<853; $i++){
 
-for ($i=0; $i<853; $i++)
-    if( ($intactEntities = fetch_data('/intact?limit=1000&page='.$i)) !== false){
-
-        foreach($intactEntities->interactions as $interaction){
-            $tmp = $interaction->interactor_a->accession . ',' . $interaction->interactor_b->accession . ',' . $interaction->confidence;
-            if ( !in_array($tmp, $ppi_data) )
-                $ppi_data[] = $tmp;
-        }
-
+    $intactEntities = json_decode(file_get_contents("../data/intact/".$i.'.json'));
+    foreach($intactEntities->interactions as $interaction){
+        $tmp = $interaction->interactor_a->accession . ',' . $interaction->interactor_b->accession . ',' . $interaction->confidence;
+        fwrite($export_file, $tmp . "\n");
     }
-
-foreach($ppi_data as $ppi)
-    fwrite($export_file, $ppi . "\n");
-
-//print_r($ppi_data);
+}
 
 fclose($export_file);
 
