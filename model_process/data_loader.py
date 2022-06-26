d63c703c9720571fe99f9a975aa72a5160de4d26
Samet <37181660+Sametle06@users.noreply.github.com>
2022-06-26 20:48:51 +0300

    docstring

--- model_process/data_loader.py
+++ model_process/data_loader.py
@@ -39,6 +39,30 @@ def get_data(file_X,file_y, ratio = 0.2, normal = True, feat_ext = None,
              threshold = 0.95,
              k = 100):
     
+    """
+    Description:
+        Function loads feature and label matrix and splitting to train and 
+        test set. Preprocessing (normalization, feature extraction) can be 
+        done respect to parameters.
+    Parameters:
+        file_X: {str}, file name of feature matrix
+        file_y: {str}. file name of label matrix
+        ratio: {float}, default = 0.2, ratio of test size to whole data size
+        normal: {bool}, default = True, If True, feature based normalization
+            is done
+        feat_ext: {str}, {'pca', 'chi2', 'f_classif', 'mutual_info_classif'},
+            default = None, If None, no extraction is done.
+        threshold: {float}, default = 0.95, threshold variance ratio of PCA 
+            matrix. Until limit is satisfied, dimensions are returned.
+        k: {int}, default = 100, select best k features according to values.
+    Return:
+        X_train: {numpy array}, train feature matrix
+        X_test: {numpy array}, test feature matrix
+        y_train: {numpy array}, train label matrix
+        y_test: {numpy array}, test label matrix
+    
+    """
+    
     X,y = loader(file_X,file_y)
     
     if feat_ext is not None:
